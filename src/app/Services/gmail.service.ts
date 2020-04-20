f299581234936fd2cd7b26f31114d43fb4de669f
pafllamero <pafllamero@gmail.com>
2020-04-20 18:23:22 +0200

    Servicios http

--- src/app/Services/gmail.service.ts
+++ src/app/Services/gmail.service.ts
@@ -9,40 +9,40 @@ export class GmailService {
 
   constructor(private http: HttpClient, private login: LoginService) { }
 
-  public getRecibidos = function () {
-    const url = "https://www.googleapis.com/gmail/v1/users/"+this.login.userId+"/messages?";
+  public getRecibidos = function() {
+    const url = 'https://www.googleapis.com/gmail/v1/users/' + this.login.userId + '/messages?';
     const authToken = this.login.tokenUser;
-    let headers = new HttpHeaders({ Authorization: `Bearer ${authToken}`});
+    const headers = new HttpHeaders({ Authorization: `Bearer ${authToken}`});
     return this.http.get(url, { headers } );
   };
 
-  public getMessage = function (id: string) {
-    const url = "https://www.googleapis.com/gmail/v1/users/"+this.login.userId+"/messages/"+id;
+  public getMessage = function(id: string) {
+    const url = 'https://www.googleapis.com/gmail/v1/users/' + this.login.userId + '/messages/' + id;
     const authToken = this.login.tokenUser;
-    let headers = new HttpHeaders({ Authorization: `Bearer ${authToken}`});
+    const headers = new HttpHeaders({ Authorization: `Bearer ${authToken}`});
 
     let params = new HttpParams();
     params = params.append('format', 'full');
 
-    return this.http.get(url, { headers:headers, params: params } );
+    return this.http.get(url, { headers, params } );
   };
 
-  public sendMessage = function (text: string, to: string, subject: string){
-    const url = "https://www.googleapis.com/gmail/v1/users/"+this.login.userId+"/messages/send";
+  public sendMessage = function(text: string, to: string, subject: string) {
+    const url = 'https://www.googleapis.com/gmail/v1/users/' + this.login.userId + '/messages/send';
     const authToken = this.login.tokenUser;
 
-    let headers = new HttpHeaders({
+    const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       Authorization: `Bearer ${authToken}`
     });
 
-    const emailTemplate = 
-      "Content-Type:  text/plain; charset=\"UTF-8\"\nContent-length: 5000\nContent-Transfer-Encoding: message/rfc2822\n" +
-      `To: ${to}\n`+
-      `Subject: ${subject}\n\n`+
+    const emailTemplate =
+      'Content-Type:  text/plain; charset="UTF-8"\nContent-length: 5000\nContent-Transfer-Encoding: message/rfc2822\n' +
+      `To: ${to}\n` +
+      `Subject: ${subject}\n\n` +
       `${text}`;
     const base64EncodedEmail = btoa(emailTemplate).replace(/\+/g, '-').replace(/\//g, '_');
 
-    return this.http.post(url, { 'raw': base64EncodedEmail }, { headers: headers } );
-  }
+    return this.http.post(url, { raw: base64EncodedEmail }, { headers } );
+  };
 }
--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -1,34 +1,34 @@
-import { Injectable, NgZone } from "@angular/core";
-import * as _ from "lodash";
-import { GoogleAuthService } from "ng-gapi/lib/GoogleAuthService";
+import { Injectable, NgZone } from '@angular/core';
+import * as _ from 'lodash';
+import { GoogleAuthService } from 'ng-gapi/lib/GoogleAuthService';
 import GoogleUser = gapi.auth2.GoogleUser;
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  public static readonly SESSION_STORAGE_KEY: string = "usuarioGoogle";
+  public static readonly SESSION_STORAGE_KEY: string = 'usuarioGoogle';
 
   profile: any = undefined;
   tokenUser: string;
   userId: string;
 
-  constructor(private googleAuthService: GoogleAuthService, private ngZone: NgZone) { 
-    if(this.isUserSignedIn()){
+  constructor(private googleAuthService: GoogleAuthService, private ngZone: NgZone) {
+    if (this.isUserSignedIn()) {
       this.setUser(this.getSessionUser());
     }
   }
 
-  private setUser(user: any){
-    this.profile = user['w3'];
-    this.tokenUser = user['Zi'].access_token;
-    this.userId = this.profile['Eea'];
+  private setUser(user: any) {
+    this.profile = user.w3;
+    this.tokenUser = user.Zi.access_token;
+    this.userId = this.profile.Eea;
   }
 
   public getSessionUser(): GoogleUser {
-    let user: string = sessionStorage.getItem(LoginService.SESSION_STORAGE_KEY);
+    const user: string = sessionStorage.getItem(LoginService.SESSION_STORAGE_KEY);
     if (!user) {
-      throw new Error("no token set , authentication required");
+      throw new Error('no token set , authentication required');
     }
     return JSON.parse(user);
   }
@@ -71,4 +71,4 @@ export class LoginService {
   private signInErrorHandler(err) {
     console.warn(err);
   }
-} 
+}
